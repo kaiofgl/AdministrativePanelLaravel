0d31d737c27d2e63b70468eb12f84aa6d87d9b30
kaiofelipegl <kaiolazaro36@hotmail.com>
2021-09-05 23:13:12 -0300

    Criação das Migrations de Funcionário e Empresa

---
+++ database/migrations/2021_09_06_015237_create_employee_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employee', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name',100);
+            $table->string('email',100);
+            $table->char('phone',15);
+            $table->char('cpf',11);
+            $table->foreignId('company_id')->constrained('company');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employee');
+    }
+}
