0dca3ab1bcda66aeec3131f7d68b5cd61ccc8b85
kaiofelipegl <kaiolazaro36@hotmail.com>
2021-09-11 18:40:01 -0300

    Criação da empresa no banco de dados com regras

--- resources/views/partials/admin/companies/addCompanies.blade.php
+++ resources/views/partials/admin/companies/addCompanies.blade.php
@@ -1,1 +1,33 @@
-<h1>ADICIONAR EMPRESAS</h1>
+<div class="container__companies-employes">
+    <div class="container__companies-employes__form">
+        <div class="container__companies-employes__title">
+            <h1>ADICIONAR EMPRESAS</h1>
+        </div>
+        <form method="POST" action="{{ route('admin.companies.add.do') }}" id="form__companies-employes" enctype="multipart/form-data"> 
+            @csrf
+
+            @if($message = Session::get('success'))
+                <div class="success_alert">{{ $message }}</div>
+            @endif
+
+            @foreach($errors->all() as $error)
+                <div class="error_alert">{{ $error }}</div>
+            @endforeach
+            <label for="name">Nome</label>
+            <input type="text" name="name" id="name">
+
+            <label for="email">E-mail</label>
+            <input type="email" name="email" id="email">
+
+            <label for="website">Website</label>
+            <input type="text" name="website" id="website">
+            @if($message = Session::get('error_file'))
+                <div class="success_alert">{{ $message }}</div>
+            @endif
+            <label for="logo">Logo</label>
+            <input type="file" name="logo" id="logo">
+            
+            <button type="submit">Cadastrar</button>
+        </form>
+    </div>
+</div>
--- resources/views/partials/admin/employees/addEmployees.blade.php
+++ resources/views/partials/admin/employees/addEmployees.blade.php
@@ -1,1 +1,24 @@
-<h1>ADICIONAR FUNCIONÁRIOS</h1>
+<div class="container__companies-employes">
+    <div class="container__companies-employes__form">
+        <div class="container__companies-employes__title">
+            <h1>ADICIONAR FUNCIONARIO</h1>
+        </div>
+        <form action="" id="form__companies-employes">
+            @csrf
+
+            <label for="">Nome</label>
+            <input type="text" name="" id="">
+
+            <label for="">E-mail</label>
+            <input type="email" name="" id="">
+
+            <label for="">Website</label>
+            <input type="text" name="" id="">
+
+            <label for="">Logo</label>
+            <input type="file" name="" id="">
+            
+            <button type="submit">Cadastrar</button>
+        </form>
+    </div>
+</div>
